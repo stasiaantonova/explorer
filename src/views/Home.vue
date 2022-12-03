338b2d73adb6c70e37bf4a05383c0bc627e7aeca
stasiaantonova <109141998+stasiaantonova@users.noreply.github.com>
2022-12-03 16:19:47 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <full-header />
     <b-link>
       <div class="d-flex justify-content-center align-items-center">
-        <appLogoImage: '/logo.svg'>
+        <vuexy-logo />
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
